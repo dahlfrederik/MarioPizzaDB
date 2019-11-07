662e9f3f7f9b38c11617c93eaa3edaaf3402c16e
Frederik Dahl <fdah@itu.dk>
2019-11-07 11:56:56 +0100

    Gitignore ish

--- src/main/java/datasource/DatabaseConnector.java
+++ src/main/java/datasource/DatabaseConnector.java
@@ -14,7 +14,7 @@ public class DatabaseConnector {
     //private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/pizza";
     private static final String USER = "root";
-    private static final String PASSWORD = "pik";
+    private static final String PASSWORD = "green8house17";
     private static Connection con;
 
     public static Connection getConnection() {
