a6146f9ef7351130e7bede71688fcf018f1b33aa
thorchris <cph-tc147@cphbusiness.dk>
2019-11-08 10:58:10 +0100

    nye filer

---
+++ Scripts/InsertData.sql
@@ -0,0 +1,23 @@
+INSERT INTO pizzas(nr, type, price) VALUES ('1', 'Margarita', '45'); 
+INSERT INTO pizzas(nr, type, price) VALUES ('2', 'Skinke', '50');
+INSERT INTO pizzas(nr, type, price) VALUES ('3', 'Pepperoni', '60');
+INSERT INTO pizzas(nr, type, price) VALUES ('4', 'Carbonara', '55'); 
+INSERT INTO pizzas(nr, type, price)  VALUES ('5', 'Silvia', '60');
+INSERT INTO pizzas(nr, type, price)  VALUES ('6', 'Amerikaner', '69');
+INSERT INTO pizzas(nr, type, price)  VALUES ('7', 'Bertil', '55');
+INSERT INTO pizzas(nr, type, price)  VALUES ('8', 'Victoria', '50');
+INSERT INTO pizzas(nr, type, price)  VALUES ('9', 'Mafia', '120');
+INSERT INTO pizzas(nr, type, price)  VALUES ('10', 'Steve Jobs', '125');
+INSERT INTO pizzas(nr, type, price)  VALUES ('11', 'Hval', '99');
+INSERT INTO pizzas(nr, type, price)  VALUES ('12', 'Chili', '45');
+INSERT INTO pizzas(nr, type, price)  VALUES ('13', 'Mario', '75');
+INSERT INTO pizzas(nr, type, price)  VALUES ('14', 'Gollum', '25');
+INSERT INTO pizzas(nr, type, price)  VALUES ('15', 'Potter', '85');
+
+INSERT INTO orders(date, nr, tele) VALUES('2019:05:21', '8', '22125052');
+INSERT INTO orders(date, nr, tele) VALUES('2019:05:02', '2', '65125312');
+INSERT INTO orders(date, nr, tele) VALUES('2019:06:15', '6', '42124212');
+INSERT INTO orders(date, nr, tele) VALUES('2019:06:26', '11', '82124256');
+INSERT INTO orders(date, nr, tele) VALUES('2019:07:10', '15', '65312421');
+
+ 
--- Scripts/MarioPizza.sql
+++ Scripts/MarioPizza.sql
@@ -1,24 +1,32 @@
-DROP TABLE if exists pizzaer;
+DROP TABLE bestillingsliste; 
+DROP TABLE ordre; 
+DROP TABLE pizzaer; 
 
-create table pizzaer (
+DROP TABLE if exists pizzas;
+
+create table pizzas (
 	nr		integer,
     type	varchar(30),
-    pris	integer,
+    price	integer,
     primary key (nr),
     UNIQUE KEY type_UNIQUE (type));
     
-DROP TABLE if exists bestillingslitste;
+    
+DROP TABLE if exists orders;
+    
+create table orders (
+	id		integer NOT NULL AUTO_INCREMENT,
+    date 	date,
+	nr		integer references pizzas,
+    tele	integer,
+    primary key (id)); 
+    
+	
+-- DROP TABLE if exists bestillingslitste;
 
-create table bestillingsliste (
-	nr		integer references pizzaer, 
-    tlf 	integer,
-    tta		integer);
-    
-DROP TABLE if exists ordre;
-    
-create table ordre (
-	dato 	integer,
-	nr		integer references pizzaer,
-    tlf		integer references bestillingsliste);
+-- create table orderlist (
+-- 	nr		integer references pizzaer, 
+--  tele 	integer references orders,
+--  ttc	integer);
     
 
