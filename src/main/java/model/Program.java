49b6538e11afc2f8180ec798af8e220ced11a5ab
Frederik Dahl <fdah@itu.dk>
2019-11-11 11:47:50 +0100

    Fixed merge

--- src/main/java/model/Program.java
+++ src/main/java/model/Program.java
@@ -3,25 +3,25 @@ package model;
 import UI.UI;
 import datasource.DataSource;
 import datasource.DatabaseHandler;
+
 /**
  *
  * @author FrederikDahl
  */
 public class Program {
-    private final MenuCard menu = new MenuCard(); 
-    private final DatabaseHandler databaseHandler; 
-    private final UI ui; 
-    
-    
-    
-    public Program(UI ui, DatabaseHandler databaseHandler, DataSource dataSource){
-        this.menu.setMenu(dataSource.getPizzas()); 
-        this.databaseHandler = databaseHandler; 
+
+    private final MenuCard menu = new MenuCard();
+    private final DatabaseHandler databaseHandler;
+    private final UI ui;
+
+    public Program(UI ui, DatabaseHandler databaseHandler, DataSource dataSource) {
+        this.menu.setMenu(dataSource.getPizzas());
+        this.databaseHandler = databaseHandler;
         this.ui = ui;
     }
-    
+
     public void runProgram() {
-        int choice = 0; 
+        int choice = 0;
         while (choice != 5) {
             ui.println("--------------------------------Mario's Pizza System--------------------------------");
             ui.println("1) Se menukort");
@@ -31,20 +31,20 @@ public class Program {
             ui.println("5) Se statistik");
             ui.println("6) Print kvittering");
             ui.println("7) Afslut");
-            
+
             try {
                 if (choice < 1 || choice > 6) {
                     throw new NumberFormatException();
                 }
-                
+
                 switch (choice) {
                     case 1:
                         showMenucard();
-                        
+
                 }
-                
-            } catch(NumberFormatException e) {
-                
+
+            } catch (NumberFormatException e) {
+
             }
         }
     }
@@ -54,9 +54,6 @@ public class Program {
         MenuCard menuCard = new MenuCard();
         menuCard.getMenu();
 
-    
+    }
 
-    
-}
-    
 }
