61a6b81800547129d27b2898fcc69cd1242b804b
Frederik Dahl <fdah@itu.dk>
2019-11-12 23:08:24 +0100

    øh

--- src/main/java/datasource/OrderMapper.java
+++ src/main/java/datasource/OrderMapper.java
@@ -75,14 +75,16 @@ public class OrderMapper {
             String SQL2 = "INSERT INTO orders (oid, date, nr, tele) VALUES (?,?,?,?)"; 
             con = DatabaseConnector.getConnection();
             PreparedStatement ps = con.prepareStatement(SQL2);
- 
-//            ps.setInt(1, order.getOrderId()); 
-//            LocalTime tid = LocalTime.now(); 
-//            Time convTime = java.sql.Time.valueOf(tid);; 
-//            ps.setTime(2,convTime); 
-//            ps.setInt(3, order.getPizzas().get(0).getPizzaNr());
-//            ps.setInt(4, customer.getTele()); 
-//              
+            ps.setInt(1, order.getOrderId()); 
+            LocalTime tid = LocalTime.now(); 
+            Time convTime = java.sql.Time.valueOf(tid);; 
+            ps.setTime(2,convTime); 
+            ps.setInt(3, order.getPizzas().get(0).getPizzaNr());
+            ps.setInt(4, customer.getTele()); 
+            ps.executeUpdate();  
+
+/* DETTE STYKKE KODE VIRKER: HARDKODET, men får forbindelse til databasen 
+
             ps.setInt(1,5); 
             LocalTime tid = LocalTime.now(); 
             Time convTime = java.sql.Time.valueOf(tid);; 
@@ -90,8 +92,7 @@ public class OrderMapper {
             ps.setInt(3,20);     
             ps.setInt(4,26156319); 
             ps.executeUpdate();
-            
-            
+ */            
             System.out.println(order + " er tilføjet til Databasen");
             
         } catch (SQLException ex) {
@@ -100,16 +101,14 @@ public class OrderMapper {
     }   
     
     public static void main(String[] args) {
-                     
         ArrayList<Pizza> pizzas = new ArrayList(); 
         Pizza pizza = new Pizza(18,"thorc",45);
         Pizza pizza1 = new Pizza(19, "Josefffff", 60); 
         pizzas.add(pizza); 
         pizzas.add(pizza1);
         Customer customer = new Customer("Hallur", 26156319); 
-        Order order = new Order(10,pizzas); 
+        Order order = new Order(6,pizzas); 
         OrderMapper om = new OrderMapper(); 
-        
         om.insertOrders(order,customer);
     }
     
