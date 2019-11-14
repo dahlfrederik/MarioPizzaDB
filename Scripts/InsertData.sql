f5fb9a00dcd0d4a8b841166eb577f29e2b9db4c7
Frederik Dahl <fdah@itu.dk>
2019-11-14 11:36:34 +0100

    Revert "stash"
    This reverts commit 0b7bf188acdb0024299de74ba93120c62114a2c3.

---
+++ Scripts/InsertData.sql
@@ -0,0 +1,29 @@
+INSERT INTO pizzas(nr, type, price) VALUES ('1', 'Margarita', '45'); 
+INSERT INTO pizzas(nr, type, price) VALUES ('2', 'Skinke', '50');
+INSERT INTO pizzas(nr, type, price) VALUES ('3', 'Pepperoni', '60');
+INSERT INTO pizzas(nr, type, price) VALUES ('4', 'Carbonara', '55'); 
+INSERT INTO pizzas(nr, type, price)  VALUES ('5', 'Silvia', '60');
+INSERT INTO pizzas(nr, type, price)  VALUES ('6', 'Amerikaner', '69');
+INSERT INTO pizzas(nr, type, price)  VALUES ('7', 'Bertil', '55');
+INSERT INTO pizzas(nr, type, price)  VALUES ('8', 'Victoria', '50');
+INSERT INTO pizzas(nr, type, price)  VALUES ('9', 'Mafia', '120');
+INSERT INTO pizzas(nr, type, price)  VALUES ('10', 'Steve Jobs', '125');
+INSERT INTO pizzas(nr, type, price)  VALUES ('11', 'Hval', '99');
+INSERT INTO pizzas(nr, type, price)  VALUES ('12', 'Chili', '45');
+INSERT INTO pizzas(nr, type, price)  VALUES ('13', 'Mario', '75');
+INSERT INTO pizzas(nr, type, price)  VALUES ('14', 'Gollum', '25');
+INSERT INTO pizzas(nr, type, price)  VALUES ('15', 'Potter', '85');
+
+INSERT INTO orders(oid, date, tele) VALUES('1', '2019:05:12', '95124524');
+INSERT INTO odetails(oid, nr, qty) VALUES('1', '5', '2');
+INSERT INTO odetails(oid, nr, qty) VALUES('1', '2', '3');
+
+INSERT INTO orders(oid, date, tele) VALUES('2', '2018:01:15', '32124421');
+INSERT INTO odetails(oid, nr, qty) VALUES('2', '3', '1');
+
+INSERT INTO orders(oid, date, tele) VALUES('3', '2017:12:05', '82812124');
+INSERT INTO odetails(oid, nr, qty) VALUES('3', '10', '3');
+INSERT INTO odetails(oid, nr, qty) VALUES('3', '7', '1');
+INSERT INTO odetails(oid, nr, qty) VALUES('3', '13', '2');
+
+
